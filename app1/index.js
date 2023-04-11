8f9b9246147d854f026648e3bda48588073dea2a
thejaswicy <115210264+thejaswicy@users.noreply.github.com>
2023-04-11 20:44:22 +0530

    feat: add symantic versioning

---
+++ app1/index.js
@@ -0,0 +1,9 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+app.get('/', (req,res)=>{
+    res.send("home page")
+})
+app.listen(5000, ()=>{
+    console.log("server is running on port 5000");
+})
