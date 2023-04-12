6a871f16091e75946a3611dcd3a61074d8cec43f
thejaswicy <115210264+thejaswicy@users.noreply.github.com>
2023-04-12 12:50:13 +0530

    feat: add features to app1

--- applications/app1/index.js
+++ applications/app1/index.js
@@ -4,7 +4,6 @@ const app = express()
 app.get('/', (req,res)=>{
     res.send("home page")
 })
-
 app.listen(5000, ()=>{
     console.log("Welcome to Javascript, Server is running on port 5000");
 })
